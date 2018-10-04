5a65e461c87f7891e4708c7c5b2e6a29f4842e19
jordanstholmes <jordanstholmes@gmail.com>
2018-10-03 17:30:26 -0700

    Started work on VideoPlayerContainer

--- src/containers/SearchContainer.js
+++ src/containers/SearchContainer.js
@@ -2,7 +2,13 @@ import React from 'react';
 import Search from './../components/Search.js';
 import { connect } from 'react-redux';
 import handleSearchChange from '../actions/search.js';
-var SearchContainer = () => {};
+var SearchContainer = () => {
+  var mapStateToProps = {
+    
+  };
+  connect();
+  return null;
+};
 
 //TODO: define a SearchContainer component which will hook up your action
 // dispatchers with your search component props.
--- src/store/store.js
+++ src/store/store.js
@@ -2,4 +2,9 @@ import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './../reducers/main.js';
 
+var store = createStore(rootReducer, {
+  videos: [],
+  currentVideo: null
+}, applyMiddleware(thunk));
 // TODO:  Create your redux store, apply thunk as a middleware, and export it!
+export default store;
